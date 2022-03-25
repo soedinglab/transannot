54ee7de575a0a14504c1afb59ff603f4f301cbce
Mariia Zelenskaia <supermashikk@gmail.com>
2022-03-25 14:58:12 +0100

    define CommandCaller

--- data/annotate.cpp
+++ data/annotate.cpp
@@ -1,6 +1,7 @@
 #include "Parameters.h"
 #include "Debug.h"
 #include "Util.h"
+#include "CommandCaller.h"
 
 // #include "annotate.sh.h" (how do they create/ where do the get such a file?????)
 
@@ -16,6 +17,8 @@ void setAnnotateMode (Parameters *p){
 int annotate(int argc, const char **argv, const Command &command){
     Parameters &par = Parameters::getInstance();
     par.PARAM_DB_OUTPUT.addCategory(MMseqsParameter::COMMAND_EXPERT);
+
+    CommandCaller cmd;
 
     cmd.addVariable("REMOVE_TMP", par.removeTmpFiles ? "TRUE" : NULL);
     return EXIT_SUCCESS;
