edcecddaf6dbb660e1bf16fcf83baee0d20e86b4
Mariia Zelenskaia <supermashikk@gmail.com>
2022-04-10 12:37:59 +0200

    Update contamination.sh

--- data/contamination.sh
+++ data/contamination.sh
@@ -11,9 +11,19 @@ notExists() {
 
 #pre-processing
 [-z "$MMSEQS"] && echo "Please set the environment variable \$MMSEQS to your current binary" && exit 1;
-["$#" -ne 2] && echo "Please provide <queryDB> <tmp>" && exit 1;
+#["$#" -ne 2] && echo "Please provide <queryDB> <tmp>" && exit 1;
 
-"$MMSEQS" createdb "${INPUT}" "${seqTaxDB}" "${CREATEDB_PAR}" \
-    || fail "Create database died"
+if notExists "${TMP_PATH}/queryDB"; then
+    #shellcheck disable=SC
+    "$MMSEQS" createdb "${RESULTS}/plass_assembly.fas" "${TMP_PATH}/queryDB" "${CREATEDB_PAR}" \
+        || fail "Create database died"
+fi
+
 
 "$MMSEQS" taxonomy 
+
+if [ -n "$REMOVE_TMP" ]; then
+    #shellcheck disable=SC
+    echo "Remove temporary files and directories"
+    rm -f "${TMP_PATH}/contamination.sh"
+fi
