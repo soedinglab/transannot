2ef63ff5ec17e9a7fa97d830bb155e942f062a0e
Mariia Zelenskaia <supermashikk@gmail.com>
2022-04-13 10:57:31 +0200

    Update downloaddb.sh

--- data/downloaddb.sh
+++ data/downloaddb.sh
@@ -9,4 +9,12 @@ notExists() {
         [ ! -f "$1" ]
 }
 
-[-z "$MMSEQS"] && echo "Please set the environment variable \$MMSEQS to your current binary." && exit 1;
+[ -z "$MMSEQS" ] && echo "Please set the environment variable \$MMSEQS to your current binary." && exit 1;
+
+[ -z "$1"] && echo "Default UniProtKB database will be downloaded." && INPUT="UniProtKB";
+
+INPUT="$1" #database to download, default UniProtKB, see L14
+OUT_PATH="$2"
+TMP_PATH="$3"
+
+
