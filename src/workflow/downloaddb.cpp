43d4a4303741f35a1e518fb8d00d481852d64e76
Mariia Zelenskaia <supermashikk@gmail.com>
2022-04-14 15:35:20 +0200

    Update downloaddb.cpp

--- src/workflow/downloaddb.cpp
+++ src/workflow/downloaddb.cpp
@@ -4,3 +4,16 @@
 #include "FileUtil.h"
 #include "CommandCaller.h"
 // #include "downloaddb.sh.h"
+#include "LocalParameters.h"
+
+int downloaddb(int argc, const char **argv, const Command& command) {
+    LocalParameters &par = LocalParameters::getLocalInstance();
+
+    tmpDir = FileUtil::createTemporaryDirectory(tmpDir, hash);
+
+    CommandCaller cmd;
+    cmd.addVariable("TMP_PATH", tmpDir.c_str());
+    //cmd.addVariable("");
+
+    cmd.addVariable("REMOVE_TMP", par.removeTmpFiles ? "TRUE" : NULL);
+}
