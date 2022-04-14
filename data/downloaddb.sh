26b9ae4dd1512f70c6fa71760ee1683bc785719b
Mariia Zelenskaia <supermashikk@gmail.com>
2022-04-14 15:34:32 +0200

    add abspath() function and update removing of temporary files

--- data/downloaddb.sh
+++ data/downloaddb.sh
@@ -1,5 +1,4 @@
 #!/bin/sh -e
-
 fail() {
     echo "Error: $1"
     exit 1
@@ -9,16 +8,38 @@ notExists() {
         [ ! -f "$1" ]
 }
 
+#get absolute pathway function
+abspath(){
+    if [ -d "$1" ]; then
+        (cd "$1"; pwd)
+    elif [ -f "$1"]; then #if file exists
+        if [ -z "${1##*/*}" ]; then
+            echo "$(cd "${1%/*}"; pwd)/${13##*/}"
+        else
+            echo "$(pwd)/$1"
+        fi
+    elif [ -d "$(dirname "$1")" ]; then #if directory to $1 exists
+            echo "$(cd "$(dirname "$1")"; pwd)/$(basename "$1")"
+    fi
+}
+
+#pre-processing
+[ "$#" -ne 3 ] && echo "Please provide <DB to download> <outDBpath> <tmp>." && exit 1;
 [ -z "$MMSEQS" ] && echo "Please set the environment variable \$MMSEQS to your current binary." && exit 1;
 
-[ -z "$1"] && echo "Default UniProtKB database will be downloaded." && SELECTION="UniProtKB";
+SELECTION="$1"
+OUTDB="$(abspath "$2")"
+TMP_PATH="$(abspath "$3")"
 
-SELECTION="$1" #database to download, default UniProtKB, see L14
-OUTDB_PATH="$2"
-TMP_PATH="$3"
-
-if notExists "${OUTDB_PATH}/${SELECTION}.fasta.gz"
+if notExists "${OUTDB}/${SELECTION}.fasta.gz"
     #shellcheck disable=SC
-    "$MMSEQS" databases "${SELECTION}" "${OUTDB_PATH}/${SELECTION}.fasta.gz" "${TMP_PATH}/download_db.tmp"
+    "$MMSEQS" databases "${SELECTION}" "${OUTDB}/${SELECTION}.fasta.gz" "${TMP_PATH}/download_db.tmp" ${DOWNLOADDB_PAR} \
         || fail "download database died"
+fi
+
+if [ -n "$REMOVE_TMP" ]; then
+    #shellcheck disable=SC
+    echo "remove temporary files and directories"
+    rm -rf "${TMP_PATH}/download_db.tmp"
+    rm -f "${TMP_PATH}/downloaddb.sh"
 fi
