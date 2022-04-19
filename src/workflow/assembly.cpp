47af4d0ab3fd8ccfadd338225e2a83a4bbc30c9d
Mariia Zelenskaia <supermashikk@gmail.com>
2022-04-19 08:41:24 +0200

    Update assembly.cpp

--- src/workflow/assembly.cpp
+++ src/workflow/assembly.cpp
@@ -7,5 +7,5 @@
 
 const char* binary_name = "Transannot";
 const char* tool_name = "Transannot";
-const char* tool_introduction = "Transannot: An annotation pipeline that predicts functions of de novo assembled transcripts based on homology search using MMseqs2"
-std::vector<Command> commands = {};
+const char* tool_introduction = "Transannot: An annotation pipeline that predicts functions of de novo assembled transcripts based on homology search using MMseqs2";
+// std::vector<Command> commands = {};
