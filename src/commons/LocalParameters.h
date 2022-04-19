65089a892b4ccc28d599e33d891b798417bb9ffc
Mariia Zelenskaia <supermashikk@gmail.com>
2022-04-19 08:41:20 +0200

    Update LocalParameters.h

--- src/commons/LocalParameters.h
+++ src/commons/LocalParameters.h
@@ -1,4 +1,29 @@
 #ifndef LOCALPARAMETERS_H
 #define LOCALPARAMETERS_H
 
+#include <Parameters.h>
+
+class LocalParameters : public  Parameters {
+public:
+    static void initInstance() {
+        //new LocalParameters;
+    }
+
+    static LocalParameters& getLocalInstance() {
+        if (instance == NULL) {
+            initInstance();
+        }
+        return static_cast<LocalParameters&>(LocalParameters::getInstance());
+    }
+
+    std::vector<MMseqsParameter*> assembly;
+    std::vector<MMseqsParameter*> annotateworkflow;
+    std::vector<MMseqsParameter*> downloaddb;
+    std::vector<MMseqsParameter*> contaminationworkflow;
+
+    LocalParameters(LocalParameters const&);
+    ~LocalParameters() {};
+    void operator=(LocalParameters const&);
+};
+
 #endif
