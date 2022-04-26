bd1d192d383b727d3a1d60006dd5e2f0ec0e58a2
Mariia Zelenskaia <supermashikk@gmail.com>
2022-04-26 16:47:16 +0200

    update contamination.sh

--- data/contamination.sh
+++ data/contamination.sh
@@ -10,21 +10,32 @@ notExists() {
 
 #pre-processing
 [-z "$MMSEQS"] && echo "Please set the environment variable \$MMSEQS to your current binary" && exit 1;
-#["$#" -ne 2] && echo "Please provide <queryDB> <tmp>" && exit 1;
+["$#" -ne 4] && echo "Please provide <InputSeq> <targetDB> <outPath> <tmp>" && exit 1; #easytaxonomy takes fasta file as an input
+[ ! -f "$2.dbtype" ] && echo "Please make sure proper target database is provided and mmseqs database is created!" && exit 1;
+[ ! -d "$4" ] && echo "tmp directory $4 not found! tmp will be created." && mkdir -p "$4";
 
-if notExists "${TMP_PATH}/queryDB"; then
-    #shellcheck disable=SC
-    "$MMSEQS" createdb "${RESULTS}/plass_assembly.fas" "${TMP_PATH}/queryDB" ${CREATEDB_PAR} \
-        || fail "Create database died"
-fi
+INPUT="$1"
+TARGET="$2"
+OUT_PATH="$3"
+TMP_PATH="$4"
+
+#if notExists "${TARGET}.dbtype"; then
+#    #shellcheck disable=SC2086
+#    "$MMSEQS" createdb "${TARGET}" "${TMP_PATH}/target" ${CREATEDB_PAR} \
+#        || fail "create targetDB died"
+#fi
 
 #only INPUT goes to this script, everything else will be automatically generated in easytaxonomy
 #only one variable should be given
 
-"$MMSEQS" easytaxonomy 
+mkdir -p "${OUT_PATH}/taxonomyReport"
+#shellcheck disable=SC2086
+"$MMSEQS" easytaxonomy "${INPUT}" "${TARGET}" "${OUT_PATH}/taxonomyReport" "${TMP_PATH}/easy_taxonomy_tmp" ${EASYTAXONOMY_PAR} \
+        || fail "get taxonomy report died"
 
 if [ -n "$REMOVE_TMP" ]; then
     #shellcheck disable=SC
     echo "Remove temporary files and directories"
+    rm -rf "${TMP_PATH}/easy_taxonomy_tmp"
     rm -f "${TMP_PATH}/contamination.sh"
 fi
