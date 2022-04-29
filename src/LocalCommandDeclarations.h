89a65d5e74c9a671bf2cc5692f13d65e07e11c51
Mariia Zelenskaia <supermashikk@gmail.com>
2022-04-29 15:36:27 +0200

    declare createdb

--- src/LocalCommandDeclarations.h
+++ src/LocalCommandDeclarations.h
@@ -6,4 +6,5 @@ extern int assembly(int argc, const char **argv, const Command& command);
 extern int annotate(int argc, const char **argv, const Command& command);
 extern int contamination(int argc, const char **argv, const Command& command);
 extern int downloaddb(int argc, const char **argv, const Command& command);
+extern int createdb(int argc, const char **argv, const Command& command);
 #endif
