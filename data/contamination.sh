7fec6c85539bcf90374e6937e2b8231609b178a9
Yazhini <yazhiniarangasbt@gmail.com>
2022-05-16 18:58:37 +0200

    Update contamination.sh

--- data/contamination.sh
+++ data/contamination.sh
@@ -45,12 +45,10 @@ rm -f "${OUT_PATH}_tophit_report"
 awk '{
     if($2 < 0.8) {
         print "Input sequence may be contaminated, for more information see", "${OUT_PATH}_tophit_report_sorted", "\n"
-        break
     }
     else {
         print "No contamination detected, possible taxonomical assignment is ", $6,"\n"
         print "For more information see", "${OUT_PATH}_tophit_report_sorted", "\n"
-        break
     }
 }' "${OUT_PATH}_tophit_report_sorted" 
 
