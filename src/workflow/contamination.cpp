05bdb82f317786bca79b4803104f9b9f28606da1
Mariia Zelenskaia <supermashikk@gmail.com>
2022-05-20 17:28:31 +0200

    include LocalPar

--- src/workflow/contamination.cpp
+++ src/workflow/contamination.cpp
@@ -3,6 +3,7 @@
 #include "Util.h"
 #include "FileUtil.h"
 #include "CommandCaller.h"
+#include "LocalParameters.h"
 #include "contamination.sh.h"
 
 int contamination(int argc, const char **argv, const Command& command) {
