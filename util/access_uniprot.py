e120f0548c776f91013234b08067b908f9611ac3
Yazhini <yazhiniarangasbt@gmail.com>
2022-05-23 14:28:23 +0200

    Update access_uniprot.py

--- util/access_uniprot.py
+++ util/access_uniprot.py
@@ -14,6 +14,6 @@ def get_url(url, **kwargs):
     
     return(response)
   
-r = get_url(f"{website_api}/uniprotkb/search?query=P04637")
+r = get_url(f"{website_api}/uniprotkb/search?query={input}")
 data = r.json()
 go_id = data['results'][0]['uniProtKBCrossReferences']['database'=='GO']['id']
