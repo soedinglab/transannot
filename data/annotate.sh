7c3b932f2d1fb25bca070fdb02f79f7d9bd96037
Mariia Zelenskaia <supermashikk@gmail.com>
2022-05-30 16:27:44 +0200

    implement linclust for the redundancy reduction

--- data/annotate.sh
+++ data/annotate.sh
@@ -23,19 +23,29 @@ INPUT="$1" #assembled sequence
 TARGET="$2"  #already downloaded datbase
 RESULTS="$3"
 TMP_PATH="$4" 
- 
+
+#MMSEQS2 LINCLUST for the redundancy reduction
+if notExists "${TMP_PATH}/clu.dbtype"; then
+	#shellcheck disable=SC2086
+	"$MMSEQS" linclust "${INPUT}" "${TMP_PATH}/clu" "${TMP_PATH}/clu_tmp" ${CLUSTER_PAR} \
+		|| fail "linclust died"
+
+	#shellcheck disable=SC2086
+	"$MMSEQS" result2repseq "${INPUT}" "${TMP_PATH}/clu" "${TMP_PATH}/clu_rep" ${RESULT2REPSEQ_PAR} \
+		|| fail "extract representative sequences died"
+fi
+
 #MMSEQS2 RBH
 #if we assemble with plass we get "${RESULTS}/plass_assembly.fas" in MMseqs db format as input
 #otherwise we have .fas file which must be translated into protein sequence and turned into MMseqs db
 #alignment DB is not a directory and may not be created
 
-#NEW: in each case we have to run RBH so i decided not to check for anything and simply run it
-#if notExists "${RESULTS}*.dbtype"; then
-
-#shellcheck disable=SC2086
-"$MMSEQS" rbh "${INPUT}" "${TARGET}" "${TMP_PATH}/alignmentDB" "${TMP_PATH}/rbh_tmp" ${SEARCH_PAR} \
-	|| fail "rbh search died"
-#fi
+#Q: should we use /clu_rep instead of query?
+if notExists "${TMP_PATH}/alignmentDB.dbtype"; then
+	#shellcheck disable=SC2086
+	"$MMSEQS" rbh "${INPUT}" "${TARGET}" "${TMP_PATH}/alignmentDB" "${TMP_PATH}/rbh_tmp" ${SEARCH_PAR} \
+		|| fail "rbh search died"
+fi
 
 #get GO-IDs
 #TO-DO think about condition to retrieve goids
@@ -66,7 +76,7 @@ esac
 #create output in .tsv format
 if notExists "${RESULTS}*.tsv"; then
 	#shellcheck disable=SC2086
-	"$MMSEQS" createtsv "${}" "${}" "${RESULTS}results.tsv" ${CREATETSV_PAR} \
+	"$MMSEQS" createtsv "${INPUT}" "${RESULTS}/go_id" "${RESULTS}/tsv_output.tsv" ${CREATETSV_PAR} \
 		|| fail "createtsv died"
 fi
 
@@ -77,6 +87,8 @@ if [ -n "${REMOVE_TMP}" ]; then
 	echo "Remove temporary files and directories"
 	rm -rf "${TMP_PATH}/annotate_tmp"  #current name of tmp pathway DO we really have annotate_tmp? or rbh_tmp? or smth else? -> we can create one tmp file for all steps and remove them at one go.
 	rm -f "${TMP_PATH}/annotate.sh"   #current name of this file	
+	#shellcheck disable=SC2086
+	"$MMSEQS" rmdb "${TMP_PATH}/clu" ${VERBOSITY_PAR}
 fi
 
 #NEW: SELECTED_INF -> which information user selected (/src/workflow/annotate.cpp)
