89e4e67344a8d3c65d6c0b8bf1699b8a9f3ef10c
Mariia Zelenskaia <supermashikk@gmail.com>
2022-06-21 15:37:28 +0200

    fix usage of plass

--- data/assemble.sh
+++ data/assemble.sh
@@ -9,24 +9,27 @@ notExists(){
 }
 
 #pre-processing
-[ -z "$PLASS" ] && echo "Please set the environment variable \$PLASS to your current binary." && exit 1;
+if ! command -v plass; then
+    echo "Please make sure that plass is installed." 
+    exit 1
+fi
 [ -z "$MMSEQS" ] && echo "Please set the environment variable \$MMSEQS to your current binary." && exit 1;
 
 INPUT="$1"
 RESULTS="$2"
 TMP_PATH="$3"
 
-mkdir -p "${TMP_PATH}/plass_tmp"
+#mkdir -p "${TMP_PATH}/plass_tmp"
 if notExists "${TMP_PATH}/plass_assembly.fas"; then
     #shellcheck disable=SC2086
-    "$PLASS" assemble "${INPUT}" "${TMP_PATH}/plass_assembly" "${TMP_PATH}/plass_tmp" ${ASSEMBLY_PAR} \
-        || fail "PLASS assembly died"
+    plass assemble "${INPUT}" "${TMP_PATH}/plass_assembly" "${TMP_PATH}/plass_tmp" ${ASSEMBLY_PAR} \
+        || fail "plass assembly died"
 fi
 
 if notExists "${RESULTS}.dbtype"; then
     echo "creating mmseqs db from assembled transcriptome"
     #shellcheck disable=SC2086
-    "$MMSEQS" createdb "${TMP_PATH}/plass_assembly.fas" "${RESULTS}"--createdb-mode 1 ${CREATEDB_PAR} \
+    "$MMSEQS" createdb "${TMP_PATH}/plass_assembly" "${RESULTS}" --createdb-mode 1 ${CREATEDB_PAR} \
         || fail "createdb died"
 fi
 
@@ -34,7 +37,6 @@ fi
 if [ -n "$REMOVE_TMP" ]; then
     #shellcheck disable=SC2086
     echo "Remove temporary files and directories"
-    rm -f "${TMP_PATH}/plass_assembly.fas"
     rm -rf "${TMP_PATH}/plass_tmp"
     rm -f "${TMP_PATH}/assemble.sh" 
 fi
