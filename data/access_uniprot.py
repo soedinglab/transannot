9613d4fd78978921f6b40b4c2f74aa238290ddf6
Mariia Zelenskaia <supermashikk@gmail.com>
2022-08-05 17:00:37 +0200

    Update access_uniprot.py

--- data/access_uniprot.py
+++ data/access_uniprot.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 import numpy as np
-import pandas as pd
 import sys, requests
 
 BASE = 'http://www.uniprot.org'
@@ -8,8 +7,9 @@ KB_ENDPOINT = '/uniprot/'
 TOOL_ENDPOINT = '/uploadlists/'
 
 def map_retrieve(ids2map, source_fmt='ACC+ID',target_fmt='ACC', output_fmt='tab'):
-    if hasattr(ids2map, 'pop'):
-        ids2map = ' '.join(ids2map)
+    ids2map = ','.join(ids2map)
+    if np.size(ids2map)!=0:
+        # ids2map = ' '.join(ids2map)
         payload = { 'query': ids2map,
                     'from': source_fmt,
                     'to': target_fmt,
@@ -24,7 +24,8 @@ def map_retrieve(ids2map, source_fmt='ACC+ID',target_fmt='ACC', output_fmt='tab'
     else:
         response.raise_for_status()
 
-uniprot_ids = pd.read_csv(sys.argv[-1], index_col=False).to_numpy() #command line arguments passed to script -> only one input in the script
+uniprot_ids = open(sys.argv[-1], "r").read().splitlines() #command line arguments passed to script -> only one input in the script
 print(uniprot_ids)
 uniprot_acc = map_retrieve(uniprot_ids, source_fmt='ACC+ID')
+print(uniprot_acc)
 sys.stdout.write(str(uniprot_acc)+'\n')
