d4e6d1242da7930961264c36b2525e1fc12c039a
Mariia Zelenskaia <supermashikk@gmail.com>
2022-08-29 14:34:29 +0200

    Update access_uniprot.py

--- data/access_uniprot.py
+++ data/access_uniprot.py
@@ -32,7 +32,7 @@ print(type(uniprot_ids))
 # uniprot_ids = open(sys.argv[-1], "r").read().splitlines() #command line arguments passed to script -> only one input in the script
 # print(uniprot_ids)
 
-seq_ident = uniprot_ids[:,2]
+seq_ident = uniprot_ids.iloc[:,2]
 print("seq_ident:")
 print(seq_ident)
 print(np.median(seq_ident))
