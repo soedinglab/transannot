aa8bb13c878b50261aa58a3327cdf51f86728056
Mariia Zelenskaia <supermashikk@gmail.com>
2022-08-29 11:01:02 +0200

    Update access_uniprot.py

--- data/access_uniprot.py
+++ data/access_uniprot.py
@@ -26,7 +26,7 @@ import sys, requests
 
 uniprot_ids = open(sys.argv[-1], "r").read().splitlines() #command line arguments passed to script -> only one input in the script
 # print(uniprot_ids)
-seq_ident = uniprot_ids[:][2]
+seq_ident = uniprot_ids[2][:]
 print(seq_ident)
 print(np.median(seq_ident))
 # uniprot_acc = map_retrieve(uniprot_ids, source_fmt='ACC+ID')
