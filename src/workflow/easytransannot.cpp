85af7126cefe1eba5a98f7895dfac355f04cd0e7
Mariia Zelenskaia <supermashikk@gmail.com>
2022-08-30 15:22:45 +0200

    add parameters for easy module

--- src/workflow/easytransannot.cpp
+++ src/workflow/easytransannot.cpp
@@ -13,25 +13,17 @@ int easytransannot(int argc, const char **argv, const Command& command) {
     par.parseParameters(argc, argv, command, true, 0, 0);
 
     std::string tmpDir = par.filenames.back();
-    par.filenames.pop_back();
     std::string hash = SSTR(par.hashParameter(command.databases, par.filenames, par.easytransannot));
     if (par.reuseLatest) {
         hash = FileUtil::getHashFromSymLink(tmpDir + "/latest");
     }
     tmpDir = FileUtil::createTemporaryDirectory(tmpDir, hash);
-    std::string outDB = par.filenames.back();
-    par.filenames.pop_back();
-    std::string targetDB = par.filenames.back();
-    par.filenames.pop_back();
-    // int i=0
-    // while (downloads[i].name == targetDB && downloads[i].dbtype == Parameters::DBTYPE_HMM_PROFILE) {
-    //     i++
-    // }
-    // if (i > downloads.size()) {
-    // }
+
 
     CommandCaller cmd;
-    cmd.addVariable("TMP_PATH", tmpDir.c_str());
+    cmd.addVariable("REMOVE_TMP", par.removeTmpFiles ? "TRUE" : NULL);
+    cmd.addVariable("VERBOSITY_PAR", par.createParameterString(par.onlyverbosity).c_str());
+    cmd.addVariable("THREADS_PAR", par.createParameterString(par.onlythreads).c_str());
 
     // TODO check for the DBtype Parameters::DBTYPE_HMM_PROFILE,
     return EXIT_SUCCESS;
