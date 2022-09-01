c8b9ece0f614c8d6f9d202bf9a51f41928ff0337
Mariia Zelenskaia <supermashikk@gmail.com>
2022-09-01 18:47:42 +0200

    add pre-filtering of mapping DB

--- util/download_mapping_db.sh
+++ util/download_mapping_db.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -e
 
-
+#download & extract
 wget https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/idmapping/idmapping.dat.gz
 gzip -d idmapping.dat.gz
+
+#remove duplicates
+awk '!seen[$3]++' idmapping.dat >> idmapping_prefilt.dat
+rm -f idmapping.dat
