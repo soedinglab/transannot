9ae006344ab28ee72ae0732536cda36454727a6f
Mariia Zelenskaia <supermashikk@gmail.com>
2022-09-06 13:25:37 +0200

    update pathways

--- data/assemblereads.sh
+++ data/assemblereads.sh
@@ -22,14 +22,14 @@ notExists(){
 #mkdir -p "${TMP_PATH}/plass_tmp"
 if notExists "${RESULTS}/assembly.fasta"; then
     #shellcheck disable=SC2086
-    "$(pwd)"/plass/bin/plass assemble "$@" "${TMP_PATH}/assembly.fasta" "${TMP_PATH}/plass_tmp" ${ASSEMBLY_PAR} \
+    "$(pwd)"/plass/bin/plass assemble "$@" "${TMP_PATH}/assembly" "${TMP_PATH}" ${ASSEMBLY_PAR} \
         || fail "plass assembly died"
 fi
 
 if notExists "${RESULTS}.dbtype"; then
     echo "creating mmseqs db from assembled transcriptome"
     #shellcheck disable=SC2086
-    "$MMSEQS" createdb "${TMP_PATH}/assembly.fasta" "${RESULTS}" --createdb-mode 1 ${CREATEDB_PAR} \
+    "$MMSEQS" createdb "${TMP_PATH}/assembly" "${RESULTS}" --createdb-mode 1 ${CREATEDB_PAR} \
         || fail "createdb died"
 fi
 
