b7b4101749398256efc03ae2404376cb057ac216
Mariia Zelenskaia <supermashikk@gmail.com>
2022-10-19 12:42:45 +0200

    Update search_eggnog.py

--- data/search_eggnog.py
+++ data/search_eggnog.py
@@ -5,7 +5,7 @@ import sys
 
 prof_db = pd.read_csv(sys.argv[-3], sep='\t', usecols=[0,1], names=['query','_target'])
 nog_db = pd.read_csv(sys.argv[-2], sep='\t', usecols=[1,5], names=['id','family_name'])
-prof_db=prof_db.drop(prof_db.tail(1).index,inplace=True)
+prof_db.drop(prof_db.tail(1).index,inplace=True)
 prof_id = prof_db['_target']
 
 nogidsdict=nog_db.set_index('id')['family_name'].to_dict()
