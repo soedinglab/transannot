6d64ee08a87431174e57ef81f944deb1e8788d78
Mariia Zelenskaia <supermashikk@gmail.com>
2022-11-08 15:19:39 +0100

    Refactoring

--- src/workflow/assemblereads.cpp
+++ src/workflow/assemblereads.cpp
@@ -12,12 +12,6 @@
     par.parseParameters(argc, argv, command, true, 0, 0);
     
     std::string tmpDir = par.filenames.back();
-    // std::string hash = SSTR(par.hashParameter(command.databases, par.filenames, *command.params));
-    // if (par.reuseLatest) {
-    //     hash = FileUtil::getHashFromSymLink(tmpDir + "/latest");
-    // }
-
-    // tmpDir = FileUtil::createTemporaryDirectory(tmpDir, hash);
     par.filenames.pop_back();
     std::string outDb = par.filenames.back();
     par.filenames.pop_back();
