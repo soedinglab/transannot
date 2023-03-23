9ca63f0c8212bcf895aaf6608fc0cd85fd67c5ab
Mariia Zelenskaia <supermashikk@gmail.com>
2023-03-23 16:52:46 +0100

    add second regression test

--- util/run_regression.sh
+++ util/run_regression.sh
@@ -16,4 +16,5 @@ mkdir -p "${BASEDIR}"
 awk -F'\t' -v OFS='\t' 'BEGIN{ i=0 }{ i++; } END{ if (i != 20) exit 1;}' "${BASEDIR}/resDB" \
 	|| fail "Check 1 failed"
 
-awk -F'\t' -v OFS='\t' 'FNR==NR{a[$4]=$2;next}a[$4]{}' "${DATA}/resDB_regression"
+awk -F'\t' -v OFS='\t' 'FNR==NR{a[$4]=$2;next}a[$4]{ if($0!=$0) exit 1;}' "${DATA}/resDB_regression" "${BASEDIR}/resDB" \
+	|| fail "Check 2 failed"
