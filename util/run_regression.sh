f99d2d05252907ce64503cb231e707b5586a42c3
Mariia Zelenskaia <supermashikk@gmail.com>
2023-03-29 14:11:22 +0200

    update regression test

--- util/run_regression.sh
+++ util/run_regression.sh
@@ -13,8 +13,8 @@ mkdir -p "${BASEDIR}"
 "${TRANSANNOT}" createquerydb ${DATA}/*.pep "${BASEDIR}/query" "${BASEDIR}/tmp_query"
 "${TRANSANNOT}" annotate  "${BASEDIR}/query" "${DATA}/pfamA_small" "${DATA}/eggNOG_DB_small" "${DATA}/SwissProt_small" "${BASEDIR}/resDB" "${BASEDIR}/tmp_annotate" --min-seq-id 0.5 --no-run-clust --remove-tmp-files --threads 128
 
-awk -F'\t' -v OFS='\t' 'BEGIN{ i=0 }{ i++; } END{ if (i != 20) exit 1;}' "${BASEDIR}/resDB" \
-	|| fail "Check 1 failed"
+awk -F'\t' -v OFS='\t' 'END{ if (NR != 20) exit 1;}' "${BASEDIR}/resDB" \
+	|| fail "Check 1 failed"; awk -F'\t' -v OFS='\t' 'END{print NR}' "${BASEDIR}/resDB"
 
 awk -F'\t' -v OFS='\t' 'FNR==NR{a[$4]=$2;next}a[$4]{ if($0!=$0) exit 1;}' "${DATA}/resDB_regression" "${BASEDIR}/resDB" \
 	|| fail "Check 2 failed"
