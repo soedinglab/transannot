ca315837e65cb03f703728a653fc4fe0ba6b8ab9
Mariia Zelenskaia <102167492+mariia-zelenskaia@users.noreply.github.com>
2023-03-29 12:36:47 +0200

    change tmp pathways

--- util/run_regression.sh
+++ util/run_regression.sh
@@ -10,8 +10,8 @@ BASEDIR="$3"
 
 mkdir -p "${BASEDIR}"
 
-"${TRANSANNOT}" createquerydb ${DATA}/*.pep "${BASEDIR}/query" "${BASEDIR}/tmp/query"
-"${TRANSANNOT}" annotate  "${BASEDIR}/query" "${DATA}/pfamA200" "${DATA}/eggNOG_DB200" "${DATA}/SwissProt_200" "${BASEDIR}/resDB" "${BASEDIR}/tmp/annotate" --min-seq-id 0.5 --no-run-clust --remove-tmp-files --threads 128
+"${TRANSANNOT}" createquerydb ${DATA}/*.pep "${BASEDIR}/query" "${BASEDIR}/tmp_query"
+"${TRANSANNOT}" annotate  "${BASEDIR}/query" "${DATA}/pfamA200" "${DATA}/eggNOG_DB200" "${DATA}/SwissProt_200" "${BASEDIR}/resDB" "${BASEDIR}/tmp_annotate" --min-seq-id 0.5 --no-run-clust --remove-tmp-files --threads 128
 
 awk -F'\t' -v OFS='\t' 'BEGIN{ i=0 }{ i++; } END{ if (i != 20) exit 1;}' "${BASEDIR}/resDB" \
 	|| fail "Check 1 failed"
